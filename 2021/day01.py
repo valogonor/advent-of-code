2a2206e032a8b242b747d73f42a2e97e18b0fd30
Valerie Ogonor <43195349+valogonor@users.noreply.github.com>
2021-12-01 17:25:25 -0500

    Update day01.py

--- 2021/day01.py
+++ 2021/day01.py
@@ -6,16 +6,16 @@ t = 2000
 q = deque()
 sm = 0
 for _ in range(t):
-	if len(q) == 3:
-		sm -= q.popleft()
-	num = int(input())
-	sm += num
-	q.append(num)
-	if last is None and len(q) == 3:
-		last = sm
-	elif last is None:
-		continue
-	if sm > last:
-		ans += 1
-	last = sm
+    if len(q) == 3:
+        sm -= q.popleft()
+    num = int(input())
+    sm += num
+    q.append(num)
+    if last is None and len(q) == 3:
+        last = sm
+    elif last is None:
+        continue
+    if sm > last:
+        ans += 1
+    last = sm
 print(ans)
