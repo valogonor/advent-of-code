f0f3d5eac51860984d93e8ecebe7cd4b92f0cbea
Valerie Ogonor <43195349+valogonor@users.noreply.github.com>
2021-12-03 01:43:20 -0500

    Update day03.py

--- 2021/day03.py
+++ 2021/day03.py
@@ -20,3 +20,48 @@ for i in range(n):
 gamma = int(gamma, 2)
 epsilon = int(epsilon, 2)
 print(gamma * epsilon)
+
+# Part Two
+t = 1000
+a = []
+for _ in range(t):
+    s = input()
+    a.append(s)
+b = a[:]
+n = len(a[0])
+
+def oxgen(i, a):
+    sm = 0
+    for num in a:
+        if num[i] == '1':
+            sm += 1
+    if sm >= len(a)/2:
+        return 1
+    return 0
+
+def coscrub(i, a):
+    sm = 0
+    for num in a:
+        if num[i] == '1':
+            sm += 1
+    if sm >= len(a)/2:
+        return 0
+    return 1
+
+for i in range(n):
+    if oxgen(i, a):
+        a = list(filter(lambda x: x[i] == '1', a))
+    else:
+        a = list(filter(lambda x: x[i] == '0', a))
+    if len(a) == 1:
+        break
+for i in range(n):
+    if coscrub(i, b):
+        b = list(filter(lambda x: x[i] == '1', b))
+    else:
+        b = list(filter(lambda x: x[i] == '0', b))
+    if len(b) == 1:
+        break
+oxy = int(a[0], 2)
+co = int(b[0], 2)
+print(oxy*co)
