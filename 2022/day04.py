30d0a05418d09f60df9f463ab711b1cdbfc6f5f1
Valerie Ogonor <43195349+valogonor@users.noreply.github.com>
2022-12-04 11:56:42 -0500

    Create day04.py

---
+++ 2022/day04.py
@@ -0,0 +1,26 @@
+with open('input.txt') as file:
+    lines = file.read().splitlines()
+
+def process(line):
+    first, second = line.split(',')
+    start1, end1 = map(int, first.split('-'))
+    start2, end2 = map(int, second.split('-'))
+    return start1, end1, start2, end2
+
+# Part 1
+ans = 0
+for line in lines:
+    start1, end1, start2, end2 = process(line)
+    if start1 <= start2 <= end2 <= end1:
+        ans += 1
+    elif start2 <= start1 <= end1 <= end2:
+        ans += 1
+print(ans)
+
+# Part 2
+ans = 1000
+for line in lines:
+    start1, end1, start2, end2 = process(line)
+    if start1 > end2 or start2 > end1:
+        ans -=1
+print(ans)
