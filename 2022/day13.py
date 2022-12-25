03adcdbadb294d0e8e51daf7e7a15bd2cc4f83c4
Valerie Ogonor <43195349+valogonor@users.noreply.github.com>
2022-12-25 00:13:38 -0500

    Update day13.py

--- 2022/day13.py
+++ 2022/day13.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+from functools import cmp_to_key
 
 with open('input.txt') as file:
     lines = [line.strip() for line in file.readlines()]
@@ -6,25 +7,25 @@ with open('input.txt') as file:
 def compare(a, b):
     if type(a) == type(b) == int:
         if a < b:
-            return 1
-        elif a > b:
-            return 0
-        else:
             return -1
+        elif a > b:
+            return 1
+        else:
+            return 0
     elif type(a) == type(b) == list:
         n = len(a)
         m = len(b)
-        res = -1
+        res = 0
         for i in range(min(n, m)):
             res = compare(a[i], b[i])
-            if res in [0, 1]:
+            if res:
                 break
-        if res == -1:
+        if res == 0:
             if n < m:
-                return 1
+                return -1
             elif n > m:
-                return 0
-            else: return -1
+                return 1
+            else: return 0
     elif type(a) == int:
         res = compare([a], b)
     else:
@@ -45,7 +46,7 @@ for line in lines:
     else:
         key = 'left'
         res = compare(d['left'], d['right'])
-        if res == 1:
+        if res == -1:
             ans += idx
         idx += 1
 print(ans)
@@ -58,18 +59,7 @@ for i, line in enumerate(lines):
     d[i] = eval(line)
 d[i+1] = [[2]]
 d[i+2] = [[6]]
-a = list(d.values())
-b = []
-best = a[0]
-while a:
-    for i in range(1, len(a)):
-        res = compare(best, a[i])
-        if res == 0:
-            best = a[i]
-    b.append(best)
-    a.remove(best)
-    if a:
-        best = a[0]
-i = b.index([[2]]) + 1
-j = b.index([[6]]) + 1
+a = sorted(d.values(), key=cmp_to_key(compare))
+i = a.index([[2]]) + 1
+j = a.index([[6]]) + 1
 print(i * j)
